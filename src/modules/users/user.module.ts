a3e008aa379c249de002ffc46dc5f228202687d6
ToniRakkaen <minhkhangg32@gmail.com>
2025-05-16 01:24:20 +0700

    fix dư

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "./user.entity";
-import { VariableModule } from "../variables/variable.module";
 import { UserRepository } from "./user.repository";
 import { UserService } from "./user.service";
 import { UserController } from "./user.controller";
@@ -9,7 +8,6 @@ import { UserController } from "./user.controller";
 @Module({
     imports:[
         MongooseModule.forFeature([{ name: User.name, schema: UserSchema}]),
-        VariableModule,
     ],
     providers: [UserRepository, UserService],
     exports: [UserRepository],
--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,11 +1,9 @@
 import { Injectable } from "@nestjs/common";
-import { VariableRepository } from "../variables/variable.repository";
 import { UserRepository } from "./user.repository";
 
 @Injectable()
 export class UserService{
     constructor(
-        private readonly variableRepository: VariableRepository,
         private readonly userRepository: UserRepository,
     ){}
     
