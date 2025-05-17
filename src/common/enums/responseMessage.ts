a1795d139f11b99e599592a560a5513386372e00
duchai922001 <Duchai757@gmail.com>
2025-05-17 14:58:33 +0700

    [feature] update message

--- src/common/enums/responseMessage.ts
+++ src/common/enums/responseMessage.ts
@@ -1,3 +1,6 @@
 export enum ResponseMessage {
   CREATE = 'Tạo dữ liệu thành công',
+  GET = 'Lấy dữ liệu thành công',
+  DELETE = 'Xóa dữ liệu thành công',
+  UPDATE = 'Cập nhật dữ liệu thành công',
 }
