eb5f198c830d2dae6f0c955645a5dc058ceaf2ca
ToniRakkaen <minhkhangg32@gmail.com>
2025-06-01 11:06:37 +0700

    Done cartItem chưa test api

---
+++ src/modules/cartItem/cartItem.controller.ts
@@ -0,0 +1,64 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CartItemService } from './cartItem.service';
+import { CreateCartItemDto } from './dtos/create-cartItem';
+import { createResponse } from 'src/common/helpers/response.helper';
+import { ResponseMessage } from 'src/common/enums/responseMessage';
+import { BaseQueryDto } from 'src/common/dtos/base-query.dto';
+
+@Controller('cart-items')
+export class CategoryController {
+  constructor(private readonly cartItemService: CartItemService) {}
+
+  @Post('')
+  async create(@Body() dto: CreateCartItemDto) {
+    const data = await this.cartItemService.create(dto);
+    return createResponse(HttpStatus.CREATED, data, ResponseMessage.CREATE);
+  }
+
+  @Get('')
+  async findAll(@Query() query: BaseQueryDto) {
+    const data = await this.cartItemService.findAll(query);
+    return createResponse(HttpStatus.OK, data, ResponseMessage.GET);
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    const data = await this.cartItemService.findById(id);
+    return createResponse(HttpStatus.OK, data, ResponseMessage.GET);
+  }
+
+  //   @Put(':id')
+  //   async update(@Param('id') id: string, @Body() dto: UpdateCategoryDto) {
+  //     const data = await this.cartItemService.update(id, dto);
+  //     return createResponse(HttpStatus.OK, data, ResponseMessage.UPDATE);
+  //   }
+
+  @Patch(':id/quantity')
+  async changeQuantity(@Param('id') id: string, @Body('delta') delta: number) {
+    const data = await this.cartItemService.incrementQuantity(id, delta);
+    return createResponse(HttpStatus.OK, data, ResponseMessage.UPDATE);
+  }
+
+  @Patch(':id/delete')
+  async softDelete(@Param('id') id: string, @Body('delta') delta: number) {
+    const data = await this.cartItemService.softDelete(id);
+    return createResponse(HttpStatus.OK, data, ResponseMessage.DELETE);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    const data = await this.cartItemService.delete(id);
+    return createResponse(HttpStatus.OK, data, ResponseMessage.DELETE);
+  }
+}
--- src/modules/cartItem/cartItem.repository.ts
+++ src/modules/cartItem/cartItem.repository.ts
@@ -3,6 +3,8 @@ import { InjectModel } from '@nestjs/mongoose';
 import { CartItem, CartItemDocument } from './cartItem.entity';
 import { Model } from 'mongoose';
 import { CreateCartItemDto } from './dtos/create-cartItem';
+import { BaseQueryDto } from 'src/common/dtos/base-query.dto';
+import { builderQuery } from 'src/common/helpers/query-builder.helper';
 
 @Injectable()
 export class CartItemRepository {
@@ -15,12 +17,33 @@ export class CartItemRepository {
     return new this.cartItemModel(data).save();
   }
 
-  async findAll(): Promise<CartItemDocument[]> {
-    return this.cartItemModel.find().exec();
+  async findAll(query: BaseQueryDto): Promise<CartItemDocument[]> {
+    const { filter, pagination, sort } = builderQuery(query);
+    const queryBuilder = this.cartItemModel
+      .find(filter)
+      .skip(pagination.skip)
+      .limit(pagination.limit)
+      .sort(sort as any);
+    return queryBuilder.exec();
   }
 
   async findById(id: string): Promise<CartItemDocument | null> {
     return this.cartItemModel.findById(id).exec();
+  }
+
+  async incrementQuantity(
+    id: string,
+    delta: number,
+  ): Promise<CartItemDocument | null> {
+    return this.cartItemModel
+      .findByIdAndUpdate(id, { $inc: { quantity: delta } }, { new: true })
+      .exec();
+  }
+
+  async softDelete(id: string): Promise<CartItemDocument | null> {
+    return this.cartItemModel
+      .findByIdAndUpdate(id, { status: 0 }, { new: true })
+      .exec();
   }
 
   async delete(id: string): Promise<boolean> {
