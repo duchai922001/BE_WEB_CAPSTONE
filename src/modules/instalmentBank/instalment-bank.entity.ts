b93e7e4fd8bd580f8dc5fb68f716719a5a135b96
duchai922001 <Duchai757@gmail.com>
2025-08-13 10:21:47 +0700

    build deploy BE socketio

--- src/modules/instalmentBank/instalment-bank.entity.ts
+++ src/modules/instalmentBank/instalment-bank.entity.ts
@@ -9,13 +9,12 @@ export class InstalmentBank {
   name: string;
 
   @Prop({ required: true })
-  logo: string; // URL ảnh logo
+  logo: string;
+  @Prop({ required: true })
+  interestRate: number;
 
   @Prop({ required: true })
-  interestRate: number; // ví dụ 0.012 (1.2%)
-
-  @Prop({ required: true })
-  term: number; // số tháng: 6, 9, 12
+  term: number;
 }
 
 export const InstalmentBankSchema =
