65263bacfe268c9fb0711f2608f37093fcf2dbfd
duchai922001 <Duchai757@gmail.com>
2025-08-19 17:15:17 +0700

    [hotfix] api favorite by user

--- src/modules/favorites/favorites.module.ts
+++ src/modules/favorites/favorites.module.ts
@@ -4,12 +4,14 @@ import { Favorite, FavoriteSchema } from './favorites.entity';
 import { FavoriteController } from './favorites.controller';
 import { FavoriteService } from './favorites.service';
 import { FavoriteRepository } from './favorites.repository';
+import { ProductImageModule } from '../productImage/productImage.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Favorite.name, schema: FavoriteSchema },
     ]),
+    ProductImageModule,
   ],
   controllers: [FavoriteController],
   providers: [FavoriteService, FavoriteRepository],
--- src/modules/favorites/favorites.service.ts
+++ src/modules/favorites/favorites.service.ts
@@ -1,10 +1,14 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateFavoriteDto } from './dtos/create.dto';
 import { FavoriteRepository } from './favorites.repository';
+import { ProductImageRepository } from '../productImage/productImage.repository';
 
 @Injectable()
 export class FavoriteService {
-  constructor(private readonly repo: FavoriteRepository) {}
+  constructor(
+    private readonly repo: FavoriteRepository,
+    private readonly productImageRepo: ProductImageRepository,
+  ) {}
 
   async addFavorite(dto: CreateFavoriteDto) {
     const existed = await this.repo.findOne(dto.userId, dto.productId);
@@ -12,8 +16,22 @@ export class FavoriteService {
     return this.repo.create(dto);
   }
 
-  getFavoritesByUser(userId: string) {
-    return this.repo.findAllByUser(userId);
+  async getFavoritesByUser(userId: string) {
+    const favorites = await this.repo.findAllByUser(userId);
+
+    const productIds = favorites.map((fav) => fav.productId._id.toString());
+
+    const defaultImages =
+      await this.productImageRepo.findDefaultByProductIds(productIds);
+
+    const imageMap = new Map(
+      defaultImages.map((img) => [img.productId.toString(), img.url]),
+    );
+
+    return favorites.map((fav) => ({
+      ...fav.toObject(),
+      image: imageMap.get(fav.productId._id.toString()) || null,
+    }));
   }
 
   async removeFavorite(id: string) {
